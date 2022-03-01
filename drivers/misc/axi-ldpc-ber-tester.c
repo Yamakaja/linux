8a6a8c59a0730cecb859de34ac4d0167097b7cfa
David Winter <dastw@gmx.net>
2022-03-01 23:49:31 +0100

    drivers: misc: axi-ldpc-ber-tester: Add new metrics
    This patch adds support for three new metrics:
    
    * iter_count i.e. iteration count tracking
    * failed_blocks to calculate FERs
    * last_failed to keep track of which blocks have failed. This allows the
      reproduction of these failures in software for further inspection.
    
    To facilitate the failure tracking this commit also introduces interrupt
    handling.
    
    Signed-off-by: David Winter <dastw@gmx.net>

--- drivers/misc/axi-ldpc-ber-tester.c
+++ drivers/misc/axi-ldpc-ber-tester.c
@@ -14,8 +14,10 @@
 #include <linux/debugfs.h>
 #include <linux/device.h>
 #include <linux/fpga/adi-axi-common.h>
+#include <linux/interrupt.h>
 #include <linux/io.h>
 #include <linux/kernel.h>
+#include <linux/kfifo.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
 #include <linux/of_device.h>
@@ -44,6 +46,11 @@
 
 #define AXI_BER_REG_LAST_MASK_LSB	0x0050
 
+#define AXI_BER_REG_INT_ENABLE		0x0060
+#define AXI_BER_REG_INT_CLEAR		0x0064
+#define AXI_BER_REG_INT_STATUS		0x0068
+#define AXI_BER_BIT_INT_LAST_FAILED	BIT(0)
+
 /* Output status registers */
 #define AXI_BER_REG_FINISHED_BLOCKS_LSB	0x0080
 #define AXI_BER_REG_BIT_ERRORS_LSB	0x0088
@@ -52,11 +59,36 @@
 #define AXI_BER_REG_IN_FLIGHT		0x0090
 #define AXI_BER_REG_LAST_STATUS		0x0094
 
+#define AXI_BER_REG_ITER_COUNT_LSB	0x0098
+#define AXI_BER_REG_FAILED_BLOCKS_LSB	0x00a0
+#define AXI_BER_REG_LAST_FAILED_LSB	0x00a8
+
 /* Private driver data */
 
+#define BER_FAIL_FIFO_SIZE 1024
+
+/**
+ * struct axi_ldpc_ber_tester_state - Driver data for ldpc ber tester
+ * @regs: Device base address for MMIO
+ * @lock: Secures access to device struct contents except for kfifo
+ * @irq: The IRQ ID assigned to this driver
+ * @irq_mask: The current interrupt mask configured for this device. Cached to avoid device
+ *	accesses.
+ * @collect_errors: Whether LAST_FAILED interrupts should be enabled to collect errors. This is
+ *	kept track of seperately because the driver will automatically disable
+ *	interrupts when the fifo is full, and this value is used to decide if they should be
+ *	re-enabled one some space has been freed.
+ * @last_failed_fifo: Stores up to BER_FAIL_FIFO_SIZE block IDs of blocks that failed to decode.
+ *	These are written in an interrupt handler and read with the mutex 'lock' held in the sysfs
+ *	attribute 'last_failed'.
+ */
 struct axi_ldpc_ber_tester_state {
 	void __iomem	*regs;
 	struct mutex	lock;
+	int		irq;
+	u32		irq_mask;
+	bool		collect_errors;
+	DECLARE_KFIFO(last_failed_fifo, u64, BER_FAIL_FIFO_SIZE);
 };
 
 typedef enum {
@@ -74,12 +106,20 @@ typedef enum {
 	AXI_BER_DIN_BEATS,
 	AXI_BER_SDFEC_CTRL_WORD,
 	AXI_BER_LAST_MASK,
+	AXI_BER_INT_ENABLE,
+	AXI_BER_INT_CLEAR,
+	AXI_BER_INT_STATUS,
 
 	AXI_BER_FINISHED_BLOCKS,
 	AXI_BER_BIT_ERRORS,
 
 	AXI_BER_IN_FLIGHT,
-	AXI_BER_LAST_STATUS
+	AXI_BER_LAST_STATUS,
+	AXI_BER_ITER_COUNT,
+	AXI_BER_FAILED_BLOCKS,
+	AXI_BER_LAST_FAILED,
+
+	AXI_BER_COLLECT_ERRORS
 } ber_attribute_id;
 
 static inline unsigned int ber_read(struct axi_ldpc_ber_tester_state *st, const u32 reg)
@@ -92,20 +132,18 @@ static inline void ber_write(struct axi_ldpc_ber_tester_state *st, const u32 reg
 	iowrite32(val, st->regs + reg);
 }
 
-static const u32 ber_versions[] = {
-	ADI_AXI_PCORE_VER(1, 0, 'a')
-};
-
-static const struct of_device_id axi_ldpc_ber_tester_match[] = {
-	{ .compatible = "cel,axi-ldpc-ber-tester-1.00.a", .data = &ber_versions[0] },
-	{ /* end of list */ }
-};
-MODULE_DEVICE_TABLE(of, axi_ldpc_ber_tester_of_match);
-
 struct ber_attribute {
 	struct device_attribute attr;
 	ber_attribute_id id;
 };
+
+static void axi_ldpc_ber_tester_int_enable(struct axi_ldpc_ber_tester_state *st,
+						  bool last_failed)
+{
+	st->irq_mask = AXI_BER_BIT_INT_LAST_FAILED * last_failed;
+	ber_write(st, AXI_BER_REG_INT_ENABLE, st->irq_mask);
+}
+
 
 #define to_ber_attribute(x) container_of(x, struct ber_attribute, attr)
 
@@ -120,6 +158,8 @@ struct ber_register_mapping_element {
 #define BER_REG_TYPE_LONG	BIT(1)
 #define BER_REG_TYPE_QUAD_WORD	BIT(2)
 #define BER_REG_TYPE_SUB	BIT(3)
+#define BER_REG_TYPE_FIFO	BIT(4)
+#define BER_REG_TYPE_PHONY	BIT(5)
 
 static const struct ber_register_mapping_element ber_register_mapping[] = {
 	[AXI_BER_VERSION]	= { .reg = AXI_BER_REG_VERSION,	.type = BER_REG_TYPE_HEX },
@@ -161,16 +201,27 @@ static const struct ber_register_mapping_element ber_register_mapping[] = {
 		.reg	= AXI_BER_REG_LAST_MASK_LSB,
 		.type	= BER_REG_TYPE_HEX | BER_REG_TYPE_QUAD_WORD
 	},
+	[AXI_BER_INT_ENABLE] = { .reg = AXI_BER_REG_INT_ENABLE, .type = BER_REG_TYPE_HEX },
+	[AXI_BER_INT_CLEAR] = { .reg = AXI_BER_REG_INT_CLEAR, .type = BER_REG_TYPE_HEX },
+	[AXI_BER_INT_STATUS] = { .reg = AXI_BER_REG_INT_STATUS, .type = BER_REG_TYPE_HEX },
 	[AXI_BER_FINISHED_BLOCKS] = {
 		.reg	= AXI_BER_REG_FINISHED_BLOCKS_LSB,
 		.type	= BER_REG_TYPE_LONG
 	},
-	[AXI_BER_BIT_ERRORS] = {
-		.reg	= AXI_BER_REG_BIT_ERRORS_LSB,
+	[AXI_BER_BIT_ERRORS] = { .reg = AXI_BER_REG_BIT_ERRORS_LSB, .type = BER_REG_TYPE_LONG },
+	[AXI_BER_IN_FLIGHT]	= { .reg = AXI_BER_REG_IN_FLIGHT },
+	[AXI_BER_LAST_STATUS]	= { .reg = AXI_BER_REG_LAST_STATUS, .type = BER_REG_TYPE_HEX },
+	[AXI_BER_ITER_COUNT]	= { .reg = AXI_BER_REG_ITER_COUNT_LSB, .type = BER_REG_TYPE_LONG },
+	[AXI_BER_FAILED_BLOCKS]	= {
+		.reg	= AXI_BER_REG_FAILED_BLOCKS_LSB,
 		.type	= BER_REG_TYPE_LONG
 	},
-	[AXI_BER_IN_FLIGHT]	= { .reg = AXI_BER_REG_IN_FLIGHT },
-	[AXI_BER_LAST_STATUS]	= { .reg = AXI_BER_REG_LAST_STATUS,	.type = BER_REG_TYPE_HEX }
+	[AXI_BER_LAST_FAILED] = {
+		.reg	= AXI_BER_REG_LAST_FAILED_LSB,
+		.type	= BER_REG_TYPE_LONG | BER_REG_TYPE_FIFO
+	},
+	[AXI_BER_COLLECT_ERRORS] = { .type = BER_REG_TYPE_PHONY }
+
 };
 
 static ssize_t axi_ldpc_ber_tester_show(struct device *dev,
@@ -184,6 +235,8 @@ static ssize_t axi_ldpc_ber_tester_show(struct device *dev,
 	int ret;
 	u32 data[4];
 	u64 long_data;
+	u64 last_failed[32];
+	int written = 0;
 
 	mutex_lock(&st->lock);
 
@@ -211,6 +264,23 @@ static ssize_t axi_ldpc_ber_tester_show(struct device *dev,
 		ret = sysfs_emit(buf, "0x%08x 0x%08x 0x%08x 0x%08x\n",
 				 data[0], data[1], data[2], data[3]);
 		break;
+	case BER_REG_TYPE_FIFO | BER_REG_TYPE_LONG:
+		ret = kfifo_out_peek(&st->last_failed_fifo, &last_failed[0], ARRAY_SIZE(last_failed));
+		for (i = 0; i < ret && PAGE_SIZE - written > 20; i++)
+			written += scnprintf(&buf[written], PAGE_SIZE-written, "%llu\n",
+					     last_failed[i]);
+		ret = written;
+		break;
+	case BER_REG_TYPE_PHONY:
+		switch (attr->id) {
+		case AXI_BER_COLLECT_ERRORS:
+			ret = sysfs_emit(buf, "%d\n", (int) st->collect_errors);
+			break;
+		default:
+			ret = - EINVAL;
+			break;
+		}
+		break;
 	default:
 		ret = -EINVAL;
 		break;
@@ -231,6 +301,7 @@ static ssize_t axi_ldpc_ber_tester_store(struct device *dev,
 	u32 i;
 	int ret;
 	u32 data[4];
+	u32 len;
 	u32 mask;
 	u64 long_data;
 	const char *ptr;
@@ -305,6 +376,48 @@ static ssize_t axi_ldpc_ber_tester_store(struct device *dev,
 
 		ret = count;
 		break;
+	case BER_REG_TYPE_FIFO | BER_REG_TYPE_LONG:
+		ret = kstrtou32(buf, 0, &data[0]);
+		if (ret) {
+			ret = -EINVAL;
+			break;
+		}
+		len = kfifo_len(&st->last_failed_fifo);
+		if (data[0] < len) {
+			len = data[0];
+			for (i = 0; i < len; i++)
+				kfifo_skip(&st->last_failed_fifo);
+		} else
+			kfifo_reset_out(&st->last_failed_fifo);
+
+		if (len && !(st->irq_mask & AXI_BER_BIT_INT_LAST_FAILED) && st->collect_errors)
+			axi_ldpc_ber_tester_int_enable(st, true);
+
+		ret = count;
+		break;
+
+	case BER_REG_TYPE_PHONY:
+		switch (attr->id) {
+		case AXI_BER_COLLECT_ERRORS:
+			ret = kstrtou32(buf, 0, &data[0]);
+			if (ret) {
+				ret = -EINVAL;
+				break;
+			}
+			data[0] = !!data[0];
+
+			if (data[0] != st->collect_errors)
+				axi_ldpc_ber_tester_int_enable(st, data[0]);
+
+			st->collect_errors = data[0];
+
+			ret = count;
+			break;
+		default:
+			ret = -EINVAL;
+			break;
+		}
+		break;
 	default:
 		ret = -EINVAL;
 		break;
@@ -314,11 +427,11 @@ static ssize_t axi_ldpc_ber_tester_store(struct device *dev,
 	return ret;
 }
 
-#define __BER_ATTR(_name, _id, _mode) { \
+#define __BER_ATTR(_name, _id, _mode) {								\
 	.attr = __ATTR(_name, _mode, axi_ldpc_ber_tester_show, axi_ldpc_ber_tester_store),	\
 	.id = _id										\
 }
-#define BER_ATTR(_name, _id, _mode)									\
+#define BER_ATTR(_name, _id, _mode)								\
 	struct ber_attribute dev_attr_##_name = __BER_ATTR(_name, _id, _mode)
 
 static BER_ATTR(version, AXI_BER_VERSION, S_IRUGO);
@@ -334,10 +447,17 @@ static BER_ATTR(awgn_cfg_offset, AXI_BER_AWGN_CFG_OFFSET, S_IWUSR | S_IRUGO);
 static BER_ATTR(din_beats, AXI_BER_DIN_BEATS, S_IWUSR | S_IRUGO);
 static BER_ATTR(sdfec_ctrl_word, AXI_BER_SDFEC_CTRL_WORD, S_IWUSR | S_IRUGO);
 static BER_ATTR(last_mask, AXI_BER_LAST_MASK, S_IWUSR | S_IRUGO);
+static BER_ATTR(int_enable, AXI_BER_INT_ENABLE, S_IWUSR | S_IRUGO);
+static BER_ATTR(int_clear, AXI_BER_INT_CLEAR, S_IWUSR);
+static BER_ATTR(int_status, AXI_BER_INT_STATUS, S_IRUGO);
 static BER_ATTR(finished_blocks, AXI_BER_FINISHED_BLOCKS, S_IRUGO);
 static BER_ATTR(bit_errors, AXI_BER_BIT_ERRORS, S_IRUGO);
 static BER_ATTR(in_flight, AXI_BER_IN_FLIGHT, S_IRUGO);
 static BER_ATTR(last_status, AXI_BER_LAST_STATUS, S_IRUGO);
+static BER_ATTR(iter_count, AXI_BER_ITER_COUNT, S_IRUGO);
+static BER_ATTR(failed_blocks, AXI_BER_FAILED_BLOCKS, S_IRUGO);
+static BER_ATTR(last_failed, AXI_BER_LAST_FAILED, S_IWUSR | S_IRUGO);
+static BER_ATTR(collect_errors, AXI_BER_COLLECT_ERRORS, S_IWUSR | S_IRUGO);
 
 static struct attribute *ldpc_ber_tester_attrs[] = {
 	&dev_attr_version.attr.attr,
@@ -353,13 +473,54 @@ static struct attribute *ldpc_ber_tester_attrs[] = {
 	&dev_attr_din_beats.attr.attr,
 	&dev_attr_sdfec_ctrl_word.attr.attr,
 	&dev_attr_last_mask.attr.attr,
+	&dev_attr_int_enable.attr.attr,
+	&dev_attr_int_clear.attr.attr,
+	&dev_attr_int_status.attr.attr,
 	&dev_attr_finished_blocks.attr.attr,
 	&dev_attr_bit_errors.attr.attr,
 	&dev_attr_in_flight.attr.attr,
 	&dev_attr_last_status.attr.attr,
+	&dev_attr_iter_count.attr.attr,
+	&dev_attr_failed_blocks.attr.attr,
+	&dev_attr_last_failed.attr.attr,
+	&dev_attr_collect_errors.attr.attr,
 	NULL
 };
 ATTRIBUTE_GROUPS(ldpc_ber_tester);
+
+static irqreturn_t axi_ldpc_ber_tester_irq_thread(int irq, void *dev_id)
+{
+	struct axi_ldpc_ber_tester_state *st = dev_id;
+	u64 last_failed;
+	int ret;
+
+	WARN_ON(st->irq != irq);
+
+	// Read current block index from device
+	last_failed = (u64) ber_read(st, AXI_BER_REG_LAST_FAILED_LSB) | (
+			((u64) ber_read(st, AXI_BER_REG_LAST_FAILED_LSB+4)) << 32);
+
+	// Clear interrupt, this also allows a new failure to be captured
+	ber_write(st, AXI_BER_REG_INT_CLEAR, AXI_BER_BIT_INT_LAST_FAILED);
+
+	// Save captured value to fifo. If fifo is at capacity, disable interrupt
+	ret = kfifo_put(&st->last_failed_fifo, last_failed);
+
+	if (!ret) {
+		mutex_lock(&st->lock);
+		axi_ldpc_ber_tester_int_enable(st, false);
+		mutex_unlock(&st->lock);
+	}
+
+	return IRQ_HANDLED;
+}
+
+static void axi_ldpc_ber_tester_int_disable(void *data)
+{
+	struct axi_ldpc_ber_tester_state *st = data;
+
+	axi_ldpc_ber_tester_int_enable(st, false);
+}
 
 static int axi_ldpc_ber_tester_probe(struct platform_device *pdev)
 {
@@ -367,6 +528,7 @@ static int axi_ldpc_ber_tester_probe(struct platform_device *pdev)
 	u32 magic;
 	const u32 *version_info;
 	u32 actual_version;
+	int ret;
 
 	version_info = of_device_get_match_data(&pdev->dev);
 	if (!version_info) {
@@ -384,6 +546,12 @@ static int axi_ldpc_ber_tester_probe(struct platform_device *pdev)
 	st->regs = devm_platform_ioremap_resource(pdev, 0);
 	if (IS_ERR(st->regs))
 		return PTR_ERR(st->regs);
+
+	st->irq = platform_get_irq(pdev, 0);
+	if (st->irq < 0) {
+		dev_err(&pdev->dev, "Failed to acquire irq!\n");
+		return -ENODEV;
+	}
 
 	magic = ber_read(st, AXI_BER_REG_MAGIC);
 	if (magic != AXI_BER_REG_MAGIC_VAL) {
@@ -405,8 +573,29 @@ static int axi_ldpc_ber_tester_probe(struct platform_device *pdev)
 		return -ENODEV;
 	}
 
-	return 0;
+	INIT_KFIFO(st->last_failed_fifo);
+
+	ret = devm_request_threaded_irq(&pdev->dev, st->irq, NULL,
+					axi_ldpc_ber_tester_irq_thread,
+					IRQF_ONESHOT, NULL, st);
+	if (ret) {
+		dev_err(&pdev->dev, "Failed to request threaded irq handler: %d\n", ret);
+		return -ENODEV;
+	}
+
+	axi_ldpc_ber_tester_int_enable(st, true);
+	return devm_add_action_or_reset(&pdev->dev, axi_ldpc_ber_tester_int_disable, st);
 }
+
+static const u32 ber_versions[] = {
+	ADI_AXI_PCORE_VER(1, 0, 'a')
+};
+
+static const struct of_device_id axi_ldpc_ber_tester_match[] = {
+	{ .compatible = "cel,axi-ldpc-ber-tester-1.00.a", .data = &ber_versions[0] },
+	{ /* end of list */ }
+};
+MODULE_DEVICE_TABLE(of, axi_ldpc_ber_tester_of_match);
 
 static struct platform_driver axi_ldpc_ber_tester_driver = {
 	.driver = {
